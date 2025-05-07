16dde94aa72c486fab59032b67d1a010653557aa
Allan8606 <allan-musico@hotmail.com>
2025-05-07 17:59:02 -0300

    atualizando

---
+++ Testese e Desafios/Desafios 09/untitled/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o título do livro para a busca: ");
+        var tituloLivro = leitura.nextLine();
+
+        String chave = "AIzaSyC35UljgY2ECF1FUTAaVD-F9ToCie1cV0Y";
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + tituloLivro + "&key=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
